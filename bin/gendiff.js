657d65217f7281a63dee255c2789e927f97d97ae
thanatos <seniaslams@gmail.com>
2022-05-23 17:35:16 +0500

    Fix eslint problems

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,12 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
 
-const program = new Command;
+const program = new Command();
 
 program
-.version('0.1')  
-.description('Compares two configuration files and shows a difference.') 
-.option('-f, --format <type>', 'output format', 'stylish')
-.argument('<filepath1> <filepath2>')
-
+  .version('0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .argument('<filepath1> <filepath2>');
 
 program.parse();
