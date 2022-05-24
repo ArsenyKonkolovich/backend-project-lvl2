419ee7b7bb81e67b138d0deff72373cdfc94c98c
thanatos <seniaslams@gmail.com>
2022-05-24 11:46:48 +0500

    Update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,7 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
+  .name('gendiff')
   .version('0.1')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format', 'stylish')
